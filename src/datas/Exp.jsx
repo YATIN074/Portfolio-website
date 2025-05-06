21d999340eac214491b7a2502232515ff1222cc0
YatinGoyal <yatingoel107@gmail.com>
2025-05-06 19:04:13 +0530

    exp changed

--- src/datas/Exp.jsx
+++ src/datas/Exp.jsx
@@ -1,14 +1,14 @@
 export const exp = [
     {
+        startTime:'01/2025',
+        endTime:'03/2025',
+        role:'Software Developer',
+        company:'Boozberg'
+    },
+    {
         startTime:'07/2024',
         endTime:'08/2024',
         role:'Frontend Developer',
         company:'IBM SkillsBuild Program'
     },
-    {
-        startTime:'08/2023',
-        endTime:'Present',
-        role:'Database Manager',
-        company:'Placement Cell'
-    }
 ]
